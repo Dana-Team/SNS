169a41398a58e25151f60726cde41c96b0010596
razchn <Razchn@gmail.com>
2021-01-25 16:20:19 +0200

    subnamespace controller alpha v1

--- main.go
+++ main.go
@@ -18,20 +18,18 @@ package main
 
 import (
 	"flag"
+	"github.com/Dana-Team/SNS/internals/webhooks"
 	"os"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
+	danav1alpha1 "github.com/Dana-Team/SNS/api/v1alpha1"
+	"github.com/Dana-Team/SNS/internals/controllers"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
-
-	"github.com/Dana-Team/SNS/internals/webhooks"
-
-	danav1alpha1 "github.com/Dana-Team/SNS/api/v1alpha1"
-	"github.com/Dana-Team/SNS/internals/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
